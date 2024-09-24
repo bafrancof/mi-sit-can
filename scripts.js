1e7d31d6b19af1e6412df002ddfbd1c276409ad2
bafrancof <bafrancoff@gmail.com>
2024-09-23 21:14:55 -0600

    Agregar archivos del sitio web

--- scripts.js
+++ scripts.js
@@ -7,3 +7,9 @@ window.onscroll = function() {
         navbar.classList.remove("scrolled");
     }
 };
+
+// Funcionalidad del menú de navegación
+document.getElementById('menu-toggle').onclick = function() {
+    var navList = document.getElementById('nav-list');
+    navList.classList.toggle('show'); // Cambia la clase para mostrar/ocultar
+};

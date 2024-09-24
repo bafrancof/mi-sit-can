495961f7f1df5394a86a3ae428bd13a555002a20
bafrancof <bafrancoff@gmail.com>
2024-09-23 20:25:16 -0600

    Agregar archivos del sitio web

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Cambia el color de la barra de navegación al hacer scroll
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+};
